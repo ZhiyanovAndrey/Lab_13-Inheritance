08584041026dd3518dcae5010f962836b309316c
ZhiyanovAndrey <aazhiyanov@gmail.com>
2021-12-19 16:17:47 +0300

    конструктор с 5 параметрами

--- Program.cs
+++ Program.cs
@@ -19,7 +19,8 @@ namespace Lab_13_Inheritance
  свойства get/set для доступа к полям класса;
  метод Print(), который выводит информацию о здании.
 
- Разработать класс MultiBuilding, который наследует возможности класса Building и добавляет поле этажность. В классе MultiBuilding реализовать следующие элементы:
+ Разработать класс MultiBuilding, который наследует возможности класса Building и добавляет поле этажность. 
+ В классе MultiBuilding реализовать следующие элементы:
 
  конструктор с 5 параметрами – реализует вызов конструктора базового класса;
  свойство get/set доступа к внутреннему полю класса;
@@ -65,17 +66,45 @@ namespace Lab_13_Inheritance
         }
         public void Print()
         {
-            Console.WriteLine("Здание по адресу {0} имеет\nдлину {1} м. ширину {2} м. высоту {3} м.", Adress, Length, Width, Heigth);
+            Console.WriteLine("Здание по адресу {0} имеет:\nдлину {1} м. \nширину {2} м. \nвысоту {3} м.", Adress, Length, Width, Heigth);
         }
 
-        //Параметризированный конструктор для классса.
-        public Building(int length, int width, int heigth, string adress)
+        
+        public Building(int length, int width, int heigth, string adress) //Параметризированный конструктор для классса.
         {
             Adress = adress;
             Length = length;
             Width = width;
             Heigth = heigth;
         }
+        sealed class MultiBuilding : Building
+        {
+            int floor; //Дополнительные поля для дочернего классса.
+
+            public MultiBuilding(int length, int width, int heigth, string adress, int floor) //Параметризированный конструктор для дочернего классса.
+              : base(length, width, heigth, adress)
+            {
+                Floor = floor;
+            }
+            public int Floor
+            {
+                get { return floor; }
+                set
+                {
+                    if (value > 0 && value < 100) floor = value;
+                    else Console.WriteLine("Число этажей вне диапазона от 0 до 100");
+                }
+            }
+
+            public void Print()
+            {
+                base.Print();
+                Console.Write("число этажей {0}", Floor);
+            }
+              
+
+        }
+
         class Program
         {
             static void Main(string[] args)
@@ -88,11 +117,14 @@ namespace Lab_13_Inheritance
                 Console.WriteLine("Введите высоту здания");
                 int c = Convert.ToInt32(Console.ReadLine());
                 string d = "175212, Пензенская область, город Подольск, пер. Косиора, 28";
-                Building building = new Building(a, b, c, d);
+                Console.WriteLine("Введите количество этажей");
+                int e = Convert.ToInt32(Console.ReadLine());
+                MultiBuilding building = new MultiBuilding(a, b, c, d, e);
                 building.Print();
+
 
                 Console.ReadKey();
             }
         }
     }
-    }
+}
